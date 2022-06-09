13b16014b0e17ee2b80520fc9aed06401dbfd05e
Calixto Neto <calixtoneeto@gnail.com>
2022-06-08 21:37:18 -0300

    Criação e configuração do workspace e projetos.

---
+++ cadastro/src/test/java/com/github/calixtoneto/ifood/cadastro/HelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.calixtoneto.ifood.cadastro;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class HelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
